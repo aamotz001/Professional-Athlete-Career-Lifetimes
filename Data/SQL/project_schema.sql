f0579fa4f44d7131f6f81a20b884b74f81b214ef
petenielsen93 <petenielsen93@gmail.com>
2021-07-24 17:24:35 -0400

    Added NBA data to schema/ERD.

--- Data/SQL/project_schema.sql
+++ Data/SQL/project_schema.sql
@@ -60,6 +60,26 @@ CREATE TABLE mlb_retiring (
   years_played INT
 );
 
+-- This table will contain NBA starting data
+CREATE TABLE nba_starting (
+  playerID VARCHAR PRIMARY KEY,
+  sport VARCHAR,
+  position VARCHAR,
+  start_year INT,
+  start_age INT,
+  years_played INT 
+);
+
+-- This table will contain NBA retiring data
+CREATE TABLE nba_retiring (
+  playerID VARCHAR PRIMARY KEY,
+  sport VARCHAR,
+  position VARCHAR,
+  retire_year INT,
+  retire_age INT,
+  years_played INT
+);
+
 -- This table will contain all starting data
 CREATE TABLE all_starting (
   playerID VARCHAR PRIMARY KEY,
@@ -128,6 +148,33 @@ FROM '.\Professional-Athlete-Career-Lifetimes\Data\Clean_Data\clean_retire_mlb.c
 DELIMITER ','
 CSV HEADER;
 
+COPY nba_starting(playerid, sport, position, start_year, start_age, years_played)
+FROM '.\Professional-Athlete-Career-Lifetimes\Data\Clean_Data\clean_start_nba.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY nba_retiring(playerid, sport, position, retire_year, retire_age, years_played)
+FROM '.\Professional-Athlete-Career-Lifetimes\Data\Clean_Data\clean_retire_nba.csv'
+DELIMITER ','
+CSV HEADER;
+
+
+--This deletes players from the NBA table that happen to have the same playerID as players in other sports. Deleting this few records should not impact our analysis.
+DELETE FROM nba_starting WHERE playerid = 'Mike Harris';
+DELETE FROM nba_retiring WHERE playerid = 'Mike Harris';
+DELETE FROM nba_starting WHERE playerid = 'Jason Williams';
+DELETE FROM nba_retiring WHERE playerid = 'Jason Williams';
+DELETE FROM nba_starting WHERE playerid = 'Travis Williams';
+DELETE FROM nba_retiring WHERE playerid = 'Travis Williams';
+DELETE FROM nba_starting WHERE playerid = 'Reggie Williams';
+DELETE FROM nba_retiring WHERE playerid = 'Reggie Williams';
+DELETE FROM nba_starting WHERE playerid = 'Derrick Williams';
+DELETE FROM nba_retiring WHERE playerid = 'Derrick Williams';
+DELETE FROM nba_starting WHERE playerid = 'Brandon Williams';
+DELETE FROM nba_retiring WHERE playerid = 'Brandon Williams';
+DELETE FROM nba_starting WHERE playerid = 'Mike Williams';
+DELETE FROM nba_retiring WHERE playerid = 'Mike Williams';
+
 
 
 --This code will join all of the starting data and retiring data into the all_starting and all_retiring tables respectively
@@ -138,6 +185,8 @@ INSERT INTO all_starting
  SELECT * FROM nfl_starting  
  UNION
  SELECT * FROM mlb_starting
+ UNION
+ SELECT * FROM nba_starting;
 
 INSERT INTO all_retiring
  SELECT * FROM nhl_retiring
@@ -145,6 +194,8 @@ INSERT INTO all_retiring
  SELECT * FROM nfl_retiring  
  UNION
  SELECT * FROM mlb_retiring 
+ UNION
+ SELECT * FROM nba_retiring;
 
 
 --This code will join the all_starting and all_retiring tables into one table, all_data, which will be exported for the NNM
@@ -152,7 +203,7 @@ INSERT INTO all_data
 SELECT all_starting.playerID, all_starting.sport, all_starting.position, all_starting.start_year, all_starting.start_age, all_starting.years_played, all_retiring.retire_year, all_retiring.retire_age
 FROM all_starting
 LEFT JOIN all_retiring
-ON (all_starting.playerID = all_retiring.playerID)
+ON (all_starting.playerID = all_retiring.playerID);
 
 
 --This code will be used to export the all_data table for running the NNM.
